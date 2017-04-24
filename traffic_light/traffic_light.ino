065e013d25951be82c3bc2ea67419a52fd108743
Alisdair Robertson <alisdair.w.robertson@gmail.com>
2017-04-24 20:22:49 +1000

    Finish Traffic Light

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -5,8 +5,14 @@ Controlling 5 LED's with a push button as input
 
 volatile bool walkButtonPressed = false;
 
+unsigned long currentMillis;
+
 unsigned int interval = 5000;   // 5 seconds
 unsigned long prevMillis;       // Global to store button state
+
+unsigned int flashInterval = 250;
+unsigned long flashPrevMillis; // Store the flash interval
+bool flashOn = true;
 
 // LED states
 int LED_TRAFFIC_RED = 12;
@@ -20,7 +26,7 @@ int LED_WALK_GREEN = 6;
 int BUTTON_PIN = 2;
 
 // States
-typedef enum {WALK_GO, WALK_STOP, TRAFFIC_GO, TRAFFIC_SLOW, TRAFFIC_STOP} state;
+typedef enum {WALK_GO, WALK_FLASH, WALK_STOP, TRAFFIC_GO, TRAFFIC_SLOW, TRAFFIC_STOP} state;
 state lights = TRAFFIC_GO;
 
 /*
@@ -29,14 +35,14 @@ state lights = TRAFFIC_GO;
 void
 buttonInterrupt()
 {
-    //Serial.println("Interrupt");
+    // Detach this interrupt
+    detachInterrupt(digitalPinToInterrupt(BUTTON_PIN));
+
+    Serial.println("Interrupt");
     // Set up the timer
     walkButtonPressed = true;
     prevMillis = millis();
     interval = 5000; // 5 seconds
-
-    // Detach this interrupt
-    detachInterrupt(digitalPinToInterrupt(BUTTON_PIN));
 }
 
 /*
@@ -46,12 +52,12 @@ void
 timerTick()
 {
     // Get snapshot of time
-    unsigned long currentMillis = millis();
+    currentMillis = millis();
 
     if (((unsigned long) currentMillis - prevMillis) >= interval){
 
         // Set up a walk button timer if one has been requested by a button press
-        if (walkButtonPressed){
+        if (walkButtonPressed && lights == TRAFFIC_GO){
             Serial.println("Walk pressed, in timer");
             walkButtonPressed = false;
             // Timer reached, and the walking button has been pressed, set the traffic to SLOW
@@ -59,16 +65,19 @@ timerTick()
             lights = TRAFFIC_SLOW;
             Serial.println("Switch to TRAFFIC_SLOW");
             prevMillis = millis();
-            interval = 3000; // 3 secs
-            // Reattach the interrupt
-            attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonInterrupt, FALLING);
-
+            interval = 1500; // 1 secs
         } else {
             // Set the next state based on the previous state
             // And for how long that state should remain the active state
             switch (lights){
 
                 case WALK_GO:
+                    lights = WALK_FLASH;
+                    Serial.println("Switch to WALK_FLASH");
+                    prevMillis = millis();
+                    interval = 3000;
+                    break;
+                case WALK_FLASH:
                     lights = WALK_STOP;
                     Serial.println("Switch to WALK_STOP");
                     prevMillis = millis();
@@ -135,6 +144,25 @@ loop()
             digitalWrite(LED_WALK_RED, LOW);
             digitalWrite(LED_WALK_GREEN, HIGH);
             break;
+        case WALK_FLASH:
+            digitalWrite(LED_TRAFFIC_RED, HIGH);
+            digitalWrite(LED_TRAFFIC_YELLOW, LOW);
+            digitalWrite(LED_TRAFFIC_GREEN, LOW);
+            digitalWrite(LED_WALK_GREEN, LOW);
+
+            if (((unsigned long) currentMillis - flashPrevMillis) >= flashInterval){
+                flashOn = !flashOn;
+                // Decide whether walk red LED should be high or low
+                flashPrevMillis = millis();
+            }
+            if (flashOn){
+                Serial.println("FLASH ON");
+                digitalWrite(LED_WALK_RED, HIGH);
+            } else {
+                Serial.println("FLASH OFF");
+                digitalWrite(LED_WALK_RED, LOW);
+            }
+            break;
         case WALK_STOP:
         case TRAFFIC_STOP:
             digitalWrite(LED_TRAFFIC_RED, HIGH);
@@ -151,6 +179,8 @@ loop()
             digitalWrite(LED_WALK_GREEN, LOW);
             break;
         case TRAFFIC_GO:
+            // Attach the interrupt
+            attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonInterrupt, FALLING);
             digitalWrite(LED_TRAFFIC_RED, LOW);
             digitalWrite(LED_TRAFFIC_YELLOW, LOW);
             digitalWrite(LED_TRAFFIC_GREEN, HIGH);
